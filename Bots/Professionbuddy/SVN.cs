c75f024795ea638e29b00f24a7641dd1c626187a
Asphodan <afflictive@gmail.com>
2012-10-27 22:12:29 -0500

    Add ProfessionBuddy.

---
+++ Bots/Professionbuddy/SVN.cs
@@ -0,0 +1,17 @@
+﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
+// Build Date: 2012/10/26 08:35:19
+// SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
+
+namespace HighVoltz
+{
+    public partial class Svn
+    {
+        protected override string RevString
+        {
+            get
+            {
+                return "518";
+            }
+        }
+    }
+}
