d6427a3579c2d82b93575307a8dd459cbef803a3
Asphodan <afflictive@gmail.com>
2012-10-28 19:59:04 -0500

    Add AutoAngler.

---
+++ Bots/AutoAngler2/SVN.cs
@@ -0,0 +1,23 @@
+﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
+// Build Date: 2012/10/09 19:15:16
+// SVN url: https://autoangler2.googlecode.com/svn/trunk
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HighVoltz
+{
+    public partial class Svn
+    {
+        protected override string RevString
+        {
+            get
+            {
+                return "99";
+            }
+        }
+    }
+}
+
